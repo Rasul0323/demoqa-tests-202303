4cb6c00489f5f2b15d6bde0a27654e452de44abb
Rasul <rasul0301@mail.ru>
2023-03-30 15:19:27 +0300

    added Dzavtotest.java

--- src/test/java/com/demoqa/Dzavtotest.java
+++ src/test/java/com/demoqa/Dzavtotest.java
@@ -1,15 +1,33 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class Dzavtotest {
+public class Dzavtotest extends TestBase {
     @Test
     void successfulSearchTest() {
         Configuration.holdBrowserOpen=true;
-    open ("https://demoqa.com/avtomation-practice-form");
+    open ("/automation-practice-form");
+        $("#firstName").setValue("Mr Tom");
+        $("#lastName").setValue("Harison");
+        $("#userEmail").setValue("vv@aaa.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("1111111111");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(byText("July")).click();
+        $(".react-datepicker__year-select").click();
+        $(byText("1993")).click();
+        $(".react-datepicker__day--014").click();
+        $("#subjectsInput").setValue("History");
+        $("#subjectsInput").pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/images/4.jpg"));
     }
 }
