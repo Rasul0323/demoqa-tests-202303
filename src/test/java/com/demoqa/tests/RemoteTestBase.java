9856b2bd37d3ba9708d275b5dfac4bd6d7fa360f
Расул <rasul0301@mail.ru>
2023-05-17 18:46:53 +0300

    added remote tests

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -10,6 +10,12 @@ public class RemoteTestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = System.getProperty("remote");
+        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.browserSize = System.getProperty("browserSize");
+        String[] browser = System.getProperty("browser").split(":");
+        Configuration.browser = browser[0];
+        Configuration.browserVersion = browser[1];
         //Configuration.browser = "chrome";
 
 
