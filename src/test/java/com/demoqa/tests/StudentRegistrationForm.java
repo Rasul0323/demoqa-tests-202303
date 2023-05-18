41c2fd2c323ea3e3f65de6ccdf329898deb2cdaf
Расул <rasul0301@mail.ru>
2023-05-18 14:08:38 +0300

    added remote tests

--- src/test/java/com/demoqa/tests/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/tests/StudentRegistrationForm.java
@@ -2,10 +2,13 @@ package com.demoqa.tests;
 
 import com.github.javafaker.Faker;
 import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.demoqa.tests.TestData.*;
 import static com.demoqa.utils.RandomUtils.*;
+import static io.qameta.allure.Allure.step;
 
 
 public class StudentRegistrationForm extends TestBase {
@@ -25,35 +28,66 @@ public class StudentRegistrationForm extends TestBase {
             randomState = faker.options().option(states),
             randomCity = getRandomCity(randomState);
 
+    @Tag("remote")
     @Test
+    @DisplayName("Проверка тестовой формы")
     void practiceFormTest() {
 
-        registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(userGender)
-                .setNumber(userNumber)
-                .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubjects(subject)
-                .setHobby(hobbies)
-                .uploadPicture("4.jpg")
-                .setCurrentAddress(currentAddress)
-                .setState(randomState)
-                .setCity(randomCity)
-                .clickSubmit();
-
-        registrationPage.verifyRegistrationResultsModalAppears()
-                .verifyResult("Student Name", firstName + " " + lastName)
-                .verifyResult("Student Email", userEmail)
-                .verifyResult("Gender", userGender)
-                .verifyResult("Mobile", userNumber)
-                .verifyResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .verifyResult("Subjects", subject)
-                .verifyResult("Hobbies", hobbies)
-                .verifyResult("Picture", "4.jpg")
-                .verifyResult("Address", currentAddress)
-                .verifyResult("State and City", randomState + " " + randomCity);
+        step("Открываем страницу", () -> {
+            registrationPage.openPage();
+        });
+        step("Закрываем банер и футер", () -> {
+            registrationPage.removeBanners();
+        });
+        step("Вводим имя и фамилию", () -> {
+            registrationPage.setFirstName(firstName);
+            registrationPage.setLastName(lastName);
+        });
+        step("Вводим email", () -> {
+            registrationPage.setUserEmail(userEmail);
+        });
+        step("Выбираем гендер", () -> {
+            registrationPage.setGender(userGender);
+        });
+        step("Вводим номер телефона", () -> {
+            registrationPage.setNumber(userNumber);
+        });
+        step("Вводим дату рождения", () -> {
+            registrationPage.setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth);
+        });
+        step("Выбираем предметы", () -> {
+            registrationPage.setSubjects(subject);
+        });
+        step("Выбираем хобби", () -> {
+            registrationPage.setHobby(hobbies);
+        });
+        step("Загружаем картинку", () -> {
+            registrationPage.uploadPicture("duck.jpg");
+        });
+        step("Вводим адрес", () -> {
+            registrationPage.setCurrentAddress(currentAddress);
+        });
+        step("Выбираем штат", () -> {
+            registrationPage.setState(randomState);
+        });
+        step("Выбираем город", () -> {
+            registrationPage.setCity(randomCity);
+        });
+        step("Кликаем на кнопку Submit", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Проверяем результат", () -> {
+            registrationPage.verifyRegistrationResultsModalAppears()
+                    .verifyResult("Student Name", firstName + " " + lastName)
+                    .verifyResult("Student Email", userEmail)
+                    .verifyResult("Gender", userGender)
+                    .verifyResult("Mobile", userNumber)
+                    .verifyResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                    .verifyResult("Subjects", subject)
+                    .verifyResult("Hobbies", hobbies)
+                    .verifyResult("Picture", "duck.jpg")
+                    .verifyResult("Address", currentAddress)
+                    .verifyResult("State and City", randomState + " " + randomCity);
+        });
     }
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,19 +1,39 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = "chrome";
-        Configuration.timeout = 3000;
-        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
-
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
